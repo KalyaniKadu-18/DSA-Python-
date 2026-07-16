e32b042172bfd28ea572043e44feb546f4845456
Kalyani Kadu <kadukalyani18@gmail.com>
2026-07-16 19:29:55 +0530

    Remove unwanted files

--- Pig_Latin.py
+++
@@ -1,8 +0,0 @@
-word = 'Kalyani'
-vowels = "aeiou"
-
-for i in range(len(word)):
-    if word[i] in vowels:
-        result = word[i:] + word[:i] + "ay"
-        print("Result:", result)
-        break
